54b51fbf4d6819bdb1678d1765c354b74714358c
shirameirovitz <shirameirovi@gmail.com>
2021-01-21 02:15:05 +0200

    answer for q1

--- Exercises.js
+++ Exercises.js
@@ -1,40 +1,37 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
+  let backwords = str.split("").reverse().join("");
+  return backwords;
 }
 
 //Question 2
 function allCombinations(str) {
-  let combinations = [];
-  // your code here
+  
 
-  //
-  return combinations;
 }
 
 //Question 3
 function allCaps(str) {
-  // your code here
-  return "";
+  
+  
+
 }
 
 //Question 4
 function myPower(x, n) {
-  // your code here
-  return "";
+ 
+
 }
 
 //Question 5
-function getFirstNotRepeating(str) {
-  // your code here
-  return "";
+function getFirstNotRepeating(str) { //sbbaaa-> s
+  
 }
 
 //Question 6 (Bonus)
 function isPrefectNumber(num) {
-  // your code here
-  return "I'm not interested in the bonus question :(";
+  
+
 }
 
 // *** Playground ***
